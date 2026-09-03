6878b27431bec136aff6fb531ad5b15a0352b59c
Satyam Lawaniya <lawaniyasatyam9@gmail.com>
2026-09-03 23:45:22 +0530

    fix: set slideshow interval to 3s

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const WISH_LINES = [
 const NAME = 'Chhabdi'
 
 const WISH_LINE_INTERVAL = 3500
-const SLIDE_INTERVAL = 3800 + 1500 // increased by 1.5s to allow images to load
+const SLIDE_INTERVAL = 3000 // slideshow interval set to 3s per user request
 const FILTER_INTERVAL = 12000
 
 export default function App() {
